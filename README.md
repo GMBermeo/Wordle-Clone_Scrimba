d904b2b0887cceb9b3b44df02ff7a7e1b67b6642
Guilherme Bermêo <gm.bermeo@hotmail.com>
2022-04-04 13:22:03 -0300

    😔 Scrimba incomplete

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Scrimba Wordle Clone js
- https://youtu.be/ezk8HBz5NKo
+
+https://youtu.be/ezk8HBz5NKo
+
+scrimba.com/scrim/co5094206add6513d2b93a309
--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+const guessesAllowed = 6;
+const numOfLettersPerWord = 5;
+const board = document.getElementById("board");
+
+for (let i = 0; i < guessesAllowed; i++) {
+    const row = document.createElement("div")
+    board.appendChild(row)
+
+    for (let j = 0; j < numOfLettersPerWord; j++) {
+        const column = document.createElement("div")
+        row.appendChild(column)
+    }
+}
+
 function play() {
-    
+
 }
